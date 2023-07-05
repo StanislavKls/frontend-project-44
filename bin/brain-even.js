4527ca65b07cf26076e37088d7e974fa2737f9b0
Stanislav Kletsov <klecov@gmail.com>
2023-07-05 11:34:47 +0500

    step 5 fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,41 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 import hello from '../src/cli.js';
 import game from '../src/game-engine.js';
 
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+
 const isEven = (num) => num % 2 === 0;
 
+const checkAnwer = (answer, num) => {
+  let result = false;
+  if (isEven(num) && answer === 'yes') {
+    result = true;
+  }
+  if (isEven(num) && answer !== 'yes') {
+    console.log(`${answer} is wrong answer ;(. Correct answer was 'no'.`);
+    result = false;
+  }
+  if (!isEven(num) && answer === 'no') {
+    result = true;
+  }
+  if (!isEven(num) && answer !== 'no') {
+    console.log(`${answer} is wrong answer ;(. Correct answer was 'no'.`);
+    result = false;
+  }
+
+  return result;
+};
+
+const brainEven = () => {
+  const number = getRandomInt(100);
+
+  console.log(`Question: ${number}`);
+
+  const answer = readlineSync.question('Your answer: ');
+
+  return checkAnwer(answer, number);
+};
+
 const name = hello();
-game(name, isEven);
+game(name, brainEven);
--- src/game-engine.js
+++ src/game-engine.js
@@ -1,21 +1,10 @@
-import readlineSync from 'readline-sync';
-import checkAnwer from './check.js';
-
-const getRandomInt = (max) => Math.floor(Math.random() * max);
-
 export default (name, gameFun) => {
   let count = 0;
 
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   while (count < 3) {
-    const number = getRandomInt(100);
-
-    console.log(`Question: ${number}`);
-
-    const answer = readlineSync.question('Your answer: ');
-
-    if (!checkAnwer(answer, number, gameFun)) {
+    if (!gameFun()) {
       console.log(`Let's try again, ${name}!`);
       break;
     }
